e77fd976e5aab7665c579796b7ea27e17c281384
Fesposito9197 <kekkoesp8@gmail.com>
2022-11-30 12:20:07 +0100

    aggiungere search bar header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,21 @@ export default {
 </script>
 
 <template>
-  <div></div>
+  <div class="container d-flex justify-content-between mt-2">
+    <h1 class="text-danger">Boolfix</h1>
+    <form class="d-flex">
+      <input
+        class="form-control form-control-sm w-auto"
+        type="text"
+        placeholder="Search Title "
+      />
+      <button type="button" class="btn text-white btn-dark">Search</button>
+    </form>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+button {
+  margin-left: 10px;
+}
+</style>
